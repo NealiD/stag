705c94946ac9618c1a39744d671518f9bf5067ab
NealiD <danielkozel94@gmail.com>
2025-04-07 10:35:36 +0200

    konec hodiny

--- src/main/java/pro1/Main3.java
+++ src/main/java/pro1/Main3.java
@@ -1,6 +1,11 @@
 package pro1;
 
+import com.google.gson.Gson;
 import pro1.apiDataModel.ActionsList;
+import pro1.apiDataModel.Teacher;
+import pro1.apiDataModel.TeachersList;
+
+import java.util.Comparator;
 
 public class Main3 {
 
@@ -10,16 +15,28 @@ public class Main3 {
 
     public static String emailOfBestTeacher(String department, int year)
     {
+        String json1 = Api.getTeachersByDepartment(department);
+        String json2 = Api.getActionsByDepartment(department, year);
+        TeachersList teacherslist = new Gson().fromJson(json1, TeachersList.class);
+        ActionsList actionlist = new Gson().fromJson(json2, ActionsList.class);
+
+        Teacher bestTeacher = teacherslist.items.stream()
+                .max(Comparator.comparing(t -> TeacherScore(t.id,actionlist)))
+                .get();
         // TODO 3.2:
         //  - Stáhni seznam učitelů na katedře
         //  - Stáhni seznam akcí na katedře
         //  - Najdi učitele s nejvyšším "score" a vrať jeho e-mail
 
-        return "";
+        return bestTeacher.email;
     }
 
     public static long TeacherScore(long teacherId, ActionsList departmentSchedule)
     {
-        return 0; // TODO 3.1: Doplň pomocnou metodu - součet všech přihlášených studentů na akcích daného učitele
+        return departmentSchedule.items.stream()
+                .filter(a -> a.teacherId == teacherId)
+                .mapToInt(a -> a.personsCount)
+                .sum();
+        // TODO 3.1: Doplň pomocnou metodu - součet všech přihlášených studentů na akcích daného učitele
     }
 }
--- src/main/java/pro1/Main4.java
+++ src/main/java/pro1/Main4.java
@@ -1,5 +1,12 @@
 package pro1;
 
+import com.google.gson.Gson;
+import pro1.apiDataModel.Teacher;
+import pro1.apiDataModel.TeachersList;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class Main4 {
 
     public static void main(String[] args) {
@@ -8,6 +15,16 @@ public class Main4 {
 
     public static void printShortestEmails(String department, int count)
     {
+        String json1 = Api.getTeachersByDepartment(department);
+        TeachersList list = new Gson().fromJson(json1, TeachersList.class);
+
+        list.items.stream()
+                .filter(t->t.email != null)
+                .sorted(Comparator.comparing(t->t.email.length()))
+                .limit(5)
+                .forEach(t->System.out.println(t.email));
+
+
         // TODO 4.1: Vypiš do konzole "count" nejkratších učitelských emailových adres
     }
 }
